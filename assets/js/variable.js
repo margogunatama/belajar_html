fc9a93bf2f09ef341d91ffe2941883e5035039ae
Margo G <mgunatama@bankbjb.co.id>
2022-10-05 10:18:09 +0700

    node commit yarn

---
+++ assets/js/variable.js
@@ -0,0 +1,18 @@
+
+const person = {
+    fname:"Margo",
+    lname:"Gunatama",
+    age: 25
+  };
+
+  const hobby = ["Bekerja", "Tipes"];
+  const nilai = 20;
+
+  const x = 2;
+
+  if (x % 2 !==0){
+    console.log(x, "Ganjil");
+  }else{
+    console.log(x, "Genap");
+  }
+
